13ecce24a4dbdd696a0a87f18ee2ebe4860bb919
seanmcavoy <smcavoy.mcavoy@gmail.com>
2025-01-30 15:44:57 +0000

    Images added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import ButtonLogin from "@/components/ButtonLogin";
 import ListItem from "@/components/ListItem";
 import FAQListItem from "@/components/FAQListItem";
+import Image from "next/image";
+import productDemo from "./productDemo.jpeg";
 
 export default function Home() {
   const User = { name: "Sean", age: 26, address: "Annaclone" };
@@ -10,13 +12,13 @@ export default function Home() {
     "Collect customer feedback",
     "Unlimited boards",
     "Admin dashboard",
-    "24/7 support dddd",
+    "24/7 support",
   ];
   return (
     <main>
       {/* HEADER */}
       <section className="bg-base-200">
-        <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
+        <div className="max-w-5xl mx-auto flex justify-between items-center px-8 py-2">
           <div className="font-bold">CodeFastSAAS</div>
           <div className="space-x-4 max-md:hidden">
             <a className="link link-hover" href="#pricingSection">
@@ -33,16 +35,24 @@ export default function Home() {
       </section>
 
       {/* HERO */}
-      <section className="px-8 text-center py-32 max-w-3xl mx-auto">
-        <h1 className="text-4xl font-extrabold mb-6 lg:text-5xl">
-          Collect Customer feedback to build better products
-        </h1>
-        <div className="opacity-90 mb-10">
-          Create a feedback board in minutes, prioritise features, and build
-          products your customers will love.
-        </div>
+      <section className="px-8 text-center lg:text-left py-32 max-w-5xl mx-auto flex flex-col lg:flex-row gap-14 items-center lg:items-start">
+        <Image
+          src={productDemo}
+          alt="Product Demo"
+          className="w-96 rounded-xl"
+        />
 
-        <ButtonLogin isLoggedIn={isLoggedIn} User={User} />
+        <div>
+          <h1 className="text-4xl font-extrabold mb-6 lg:text-5xl">
+            Collect Customer feedback to build better products
+          </h1>
+          <div className="opacity-90 mb-10">
+            Create a feedback board in minutes, prioritise features, and build
+            products your customers will love.
+          </div>
+
+          <ButtonLogin isLoggedIn={isLoggedIn} User={User} />
+        </div>
       </section>
 
       {/* PRICING */}
