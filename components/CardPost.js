000214c4542638393af6845683e4d1bb8763fee6
seanmcavoy <smcavoy.mcavoy@gmail.com>
2025-02-11 14:33:58 +0000

    feat: button vote

--- components/CardPost.js
+++ components/CardPost.js
@@ -1,8 +1,10 @@
+import ButtonVote from "./ButtonVote";
+
 const CardPost = ({ post }) => {
   return (
     <li className="bg-base-100 rounded-3xl p-6 flex justify-between items-center">
       <div>
-        <div className="font-bold mb-1">{post.title}</div>
+        <div className="font-bold mb-1 text-lg">{post.title}</div>
         <div
           className={`opacity-80 leading-relaxed max-h-32 ${
             post.description.length > 100
@@ -13,20 +15,10 @@ const CardPost = ({ post }) => {
           {post.description}
         </div>
       </div>
-      <button className="btn btn-square">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="size-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
+      <ButtonVote
+        postId={post._id.toString()}
+        initialVotesCounter={post.votesCounter}
+      />
     </li>
   );
 };
